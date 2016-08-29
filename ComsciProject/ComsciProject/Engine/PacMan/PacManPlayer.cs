ba405b146b850dd055b4ad2a30785cc913b53fe1
Dane Horn <dane.horn7915@gmail.com>
2016-08-29 23:22:51 +0200

    Added pac-man shell

---
+++ ComsciProject/ComsciProject/Engine/PacMan/PacManPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComsciProject.Engine.PacMan
+{
+    //Controllable character
+    class PacManPlayer : Entity
+    {
+        public PacManPlayer()
+        {
+            appearance = 'C';
+            descName = "Pac-Man";
+        }
+        //Moves very slowly, not sure why
+        public override void Update()
+        {
+            Move(new Vector2(2, 0));
+        }
+    }
+}
---
+++ ComsciProject/ComsciProject/Engine/PacMan/PacWalls.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//very simple, wall appearance defined in PacManLevel
+namespace ComsciProject.Engine.PacMan
+{
+    class PacWalls : Entity
+    {
+        private PacWalls()
+        {
+
+        }
+        public PacWalls(char appearance)
+        {
+            this.appearance = appearance;
+        }
+    }
+}
